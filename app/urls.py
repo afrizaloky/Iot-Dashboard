399f5afaa050c68045074135a9837cdf8c0944f3
afrizaloky <afrizaloky@outlook.com>
2020-02-27 12:47:23 +0700

    comment

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
-    url(r'^sensors/$', ListSensorView.as_view(), name='ListSensorView'),
+    # url(r'^sensors/$', ListSensorView.as_view(), name='ListSensorView'),
     url(r'^api/$', APISensorView.as_view(), name='APISensorView'),
     # url(r'^get/$'),
 ]
